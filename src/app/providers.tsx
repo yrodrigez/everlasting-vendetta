2468fc2961b1181903a9806b4dfbab832dda8b9a
Yago <yarlorenzo@gmail.com>
2024-03-10 14:32:59 +0100

    Enhancements git ignored

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,10 @@
 'use client'
 import * as React from "react";
 
-// 1. import `NextUIProvider` component
 import {NextUIProvider} from "@nextui-org/react";
 import {ReactNode} from "react";
 
 function Providers({children}: { children: ReactNode }) {
-    // 2. Wrap NextUIProvider at the root of your app
     return (
         <NextUIProvider>
             {children}
