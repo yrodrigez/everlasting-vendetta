d573ef31bf5ed93def56ad60be2c5f149edc9cea
Yago <yarlorenzo@gmail.com>
2024-04-14 14:33:47 +0200

    Removed the development token

--- src/app/api/v1/supabase/auth/route.ts
+++ src/app/api/v1/supabase/auth/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: NextRequest) {
         method: 'POST',
         body: JSON.stringify({blizzardToken: bnetToken.value, selectedCharacter: character}),
         headers: {
-            'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'//process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+            'Authorization': 'Bearer ' + process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
         },
     });
 
