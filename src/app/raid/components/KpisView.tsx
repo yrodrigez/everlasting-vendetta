7dc4684bf21e073f8ab24aa24ab6a8a2c0848d54
Yago <yarlorenzo@gmail.com>
2024-05-04 16:21:56 +0200

    Removed the day filter as is confusing and should add the confirmed ppl only

--- src/app/raid/components/KpisView.tsx
+++ src/app/raid/components/KpisView.tsx
@@ -24,7 +24,7 @@ export function KpisView({participants, raidId, raidInProgress}: {
     const rtParticipants = useParticipants(raidId, participants)
 
     function findRoleAndDay(_participant: any, role: string, day: string) {
-        return _participant?.is_confirmed && _participant?.details?.role === role && _participant?.details?.days.find((d: string) => d.indexOf(day) >= 0)
+        return _participant?.is_confirmed && _participant?.details?.role === role
     }
 
     function findConfirmedByRole(role: string, _participants: any[], day: string) {
@@ -32,7 +32,7 @@ export function KpisView({participants, raidId, raidInProgress}: {
     }
 
     function findConfirmedByDay(day: string, _participants: any[]) {
-        return _participants.reduce((acc, participant) => acc + +(participant?.is_confirmed && !!participant?.details?.days.find((d: string) => d.indexOf(day) >= 0)), 0)
+        return _participants.reduce((acc, participant) => acc + +(participant?.is_confirmed), 0)
     }
 
     function findNotConfirmed(day: string, _participants: any[]) {
