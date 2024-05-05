e81cb631d22efd6dcc413689aed46f4b4980d22f
Yago <yarlorenzo@gmail.com>
2024-05-05 13:34:47 +0200

    Improved the fetch Item functionality, should be faster now

--- src/app/api/v1/services/wow/fetchItem/route.tsx
+++ src/app/api/v1/services/wow/fetchItem/route.tsx
@@ -1,42 +1,7 @@
 import axios from "axios";
 import {cookies} from "next/headers";
 import {type NextRequest, NextResponse} from "next/server";
-
-function getKnownItemImage(itemId: number) {
-    const knownImages = {
-        215161: 'https://wow.zamimg.com/images/wow/icons/large/inv_helmet_49.jpg',
-        210781: 'https://wow.zamimg.com/images/wow/icons/large/inv_bracer_25a.jpg',
-        211450: 'https://wow.zamimg.com/images/wow/icons/large/inv_misc_gem_pearl_07.jpg',
-        215111: 'https://wow.zamimg.com/images/wow/icons/large/inv_misc_enggizmos_27.jpg',
-        999999: 'https://wow.zamimg.com/images/wow/icons/medium/inventoryslot_empty.jpg',
-        0: 'https://wow.zamimg.com/images/wow/icons/large/inv_misc_questionmark.jpg',
-        216494: 'https://wow.zamimg.com/images/wow/icons/large/spell_shadow_demoniccircleteleport.jpg',
-        213409: 'https://wow.zamimg.com/images/wow/icons/large/inv_weapon_hand_08.jpg',
-        213350: 'https://wow.zamimg.com/images/wow/icons/large/inv_gizmo_khoriumpowercore.jpg',
-
-    } as any
-    return knownImages[itemId] || ''
-}
-
-async function fetchItemMedia(token: string, itemId: number, locale: string = 'en_US') {
-    let imageUrl = getKnownItemImage(itemId);
-    if (imageUrl) {
-        return imageUrl;
-    }
-    try {
-        const url = `https://eu.api.blizzard.com/data/wow/media/item/${itemId}?namespace=static-classic1x-eu&locale=${locale}`;
-        const {data} = await axios.get(`${url}`, {
-            headers: {'Authorization': 'Bearer ' + token}
-        })
-        imageUrl = data.assets.find((asset: any) => {
-            return asset.key === 'icon'
-        })?.value || getKnownItemImage(0)
-        return imageUrl;
-    } catch (e) {
-        //console.error('Error fetching item media:', e)
-        return getKnownItemImage(0)
-    }
-}
+import {createServerComponentClient, SupabaseClient} from "@supabase/auth-helpers-nextjs";
 
 function knownItemLevelQuality(itemId: number) {
     const knownItemLevels = {
@@ -51,7 +16,7 @@ function knownItemLevelQuality(itemId: number) {
         213350: 45,
 
     } as any
-    return knownItemLevels[itemId] || 0;
+    return knownItemLevels[itemId] ?? 0;
 
 }
 
@@ -74,18 +39,116 @@ async function fetchItemDetails(token: string, itemId: number, locale: string = 
     return itemDetails;
 }
 
+async function fetchWoWHeadItem(itemId: number) {
+    const url = `https://nether.wowhead.com/tooltip/item/${itemId}?dataEnv=4&locale=0`
+    const response = await fetch(url)
+    const data = await response.json() as {
+        icon: string,
+        quality: number,
+        name: string,
+        id: number,
+        tooltip: string
+        spells: any[]
+    }
+
+    const qualityName = [
+        'poor',
+        'common',
+        'uncommon',
+        'rare',
+        'epic',
+        'legendary',
+        'artifact',
+        'heirloom',
+    ][data.quality ?? 0]
+
+    return {
+        icon: `https://wow.zamimg.com/images/wow/icons/medium/${data.icon}.jpg`,
+        quality: data.quality,
+        qualityName: qualityName,
+        name: data.name,
+        id: data.id,
+        tooltip: data.tooltip,
+        spells: data.spells
+    }
+}
+
+async function getItemFromDatabase(supabase: SupabaseClient, itemId: number) {
+    const {data, error} = await supabase.from('wow_items')
+        .select('*')
+        .eq('id', itemId)
+        .limit(1)
+        .single()
+
+    if (error) {
+        console.error('Error fetching item from database:', error)
+        return null
+    }
+
+    if (!data?.details) {
+        return null
+    }
+
+    return {details: data.details, lastUpdated: data.updated_at}
+}
+
+async function saveItemToDatabase(supabase: SupabaseClient, itemId: number, itemDetails: any) {
+    const {data, error} = await supabase.from('wow_items')
+        .upsert({id: itemId, details: itemDetails}).select('details')
+        .limit(1)
+        .single()
+    if (error) {
+        console.error('Error saving item to database:', error)
+        return null
+    }
+
+    return data
+}
+
+async function fetchNewItem(supabase: SupabaseClient, token: { value: string, name: string }, itemId: number) {
+    const [wowHeadItem, bnetDetails] = await Promise.all([
+        fetchWoWHeadItem(itemId),
+        fetchItemDetails(token?.value, itemId)
+    ])
+
+    const itemDetails = {
+        ...bnetDetails,
+        ...wowHeadItem,
+        icon: wowHeadItem.icon,
+    }
+
+    saveItemToDatabase(supabase, itemId, itemDetails).then() // Don't wait for this to finish
+    const itemIconUrl = itemDetails.icon
+    return NextResponse.json({itemIconUrl, itemDetails})
+}
+
 export async function GET(request: NextRequest) {
     const cookieList = cookies()
     let token = cookieList.get(process.env.BNET_COOKIE_NAME!)
     const url = new URL(request.url)
     const itemId = Number(url.searchParams.get('itemId'))
     if (!token?.value) {
-
         token = {value: url.searchParams.get('token') ?? '', name: process.env.BNET_COOKIE_NAME!}
     }
 
-    const itemIconUrl = await fetchItemMedia(token?.value, itemId);
-    const itemDetails = await fetchItemDetails(token?.value, itemId);
+    if (itemId === 999999) {
+        return NextResponse.json({
+            itemIconUrl: 'https://wow.zamimg.com/images/wow/icons/medium/inventoryslot_empty.jpg',
+            itemDetails: {}
+        })
+    }
 
-    return NextResponse.json({itemIconUrl, itemDetails})
+    const supabase = createServerComponentClient({cookies})
+    const itemFromDatabase = await getItemFromDatabase(supabase, itemId)
+    const itemDetailsFromDatabase = itemFromDatabase?.details
+    const lastUpdated = itemFromDatabase?.lastUpdated
+    const updatedLessThanAWeekAgo = ((new Date().getTime() - new Date(lastUpdated).getTime()) < 1000 * 60 * 60 * 24 * 7)
+    if (itemDetailsFromDatabase && lastUpdated && updatedLessThanAWeekAgo) { // If item is in database and updated less than a week ago
+        const itemIconUrl = itemDetailsFromDatabase.icon
+        const itemDetails = itemDetailsFromDatabase;
+        
+        return NextResponse.json({itemIconUrl, itemDetails})
+    }
+
+    return fetchNewItem(supabase, token, itemId)
 }
