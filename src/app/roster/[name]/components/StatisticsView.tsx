7762830f1d9dfdd1863b7fcaa2b0711cae605f8d
Yago <yarlorenzo@gmail.com>
2024-08-12 21:06:57 +0200

    fix: removed shadow resistance as is not needed for now and added some alingments

--- src/app/roster/[name]/components/StatisticsView.tsx
+++ src/app/roster/[name]/components/StatisticsView.tsx
@@ -22,7 +22,7 @@ function Power({amount, name}: { amount: number, name: string }) {
         return <Mana className={`w-16 h-16 ${text}`}/>
     }
     return (
-        <div className="flex flex-row gap-2">
+        <div className="flex flex-row gap-2 w-40">
             <Icon/>
             <div className="flex-col flex self-end pb-1">
                 <span className={`${text} font-bold`}>{amount}</span>
@@ -66,10 +66,10 @@ export function StatisticsView({character, statistics}: { character: any, statis
 
 
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex lg:flex-col gap-4 items-center justify-center w-full">
             <div
-                className="flex lg:flex-row flex-col gap-8 w-full lg:items-center lg:justify-center">
-                <div className="flex flex-row gap-2">
+                className="flex lg:flex-row flex-col gap-8 w-full lg:items-center justify-center">
+                <div className="flex flex-row gap-2 w-40">
                     <Health className="text-red-500 w-16 h-16"/>
                     <div className="flex-col flex self-end pb-1">
                         <span className="text-red-500 font-bold">{characterStatistics.health}</span>
@@ -77,7 +77,7 @@ export function StatisticsView({character, statistics}: { character: any, statis
                     </div>
                 </div>
                 <Power amount={characterStatistics.power.amount} name={characterStatistics.power.name}/>
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-2 w-40">
                     <Stamina className="text-orange-400 w-16 h-16"/>
                     <div className="flex-col flex self-end pb-1">
                         <span className="text-orange-400 font-bold">{characterStatistics.stamina}</span>
@@ -85,49 +85,50 @@ export function StatisticsView({character, statistics}: { character: any, statis
                     </div>
                 </div>
 
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-2 w-40">
                     <FireResistance className="text-red-500 w-16 h-16"/>
                     <div className="flex-col flex self-end pb-1">
                         <span className="text-red-500 font-bold">{characterStatistics.resistances.fire}</span>
                         <span className="text-white">Fire res.</span>
                     </div>
                 </div>
-                <div className="flex flex-row gap-2">
+                {/* TODO not needed for now
+                <div className="flex flex-row gap-2 w-40">
                     <ShadowResistance className="text-purple-500 w-16 h-16"/>
                     <div className="flex-col flex self-end pb-1">
                         <span className="text-purple-500 font-bold">{characterStatistics.resistances.shadow}</span>
                         <span className="text-white">Shadow res.</span>
                     </div>
-                </div>
-                <div className="flex flex-row gap-2">
-                    <Defense className="text-gray-500 w-16 h-16"/>
-                    <div className="flex-col flex self-end pb-1">
-                        <span className="text-gray-500 font-bold">{characterStatistics.defense}</span>
-                        <span className="text-white">Defense</span>
-                    </div>
-                </div>
+                </div>*/}
+
             </div>
-            <div
-                className="flex lg:flex-row flex-col gap-8 w-full lg:items-center lg:justify-center">
-                <div className="flex flex-row gap-2">
+            <div className="flex lg:flex-row flex-col gap-8 w-full lg:items-center justify-center">
+                <div className="flex flex-row gap-2 w-40">
                     <Strength className="text-red-500 w-16 h-16"/>
                     <div className="flex-col flex self-end pb-1">
                         <span className="text-red-500 font-bold">{characterStatistics.strength}</span>
                         <span className="text-white">Strength</span>
                     </div>
                 </div>
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-2 w-40">
                     <Agility className="text-yellow-500 w-16 h-16"/>
                     <div className="flex-col flex self-end pb-1">
                         <span className="text-yellow-500 font-bold">{characterStatistics.agility}</span>
                         <span className="text-white">Agility</span>
                     </div>
                 </div>
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-2 w-40">
                     <Intellect className="text-purple-400 w-16 h-16"/>
                     <div className="flex-col flex self-end pb-1">
                         <span className="text-purple-400 font-bold">{characterStatistics.intellect}</span>
                         <span className="text-white">Intellect</span>
+                    </div>
+                </div>
+                <div className="flex flex-row gap-2 w-40">
+                    <Defense className="text-gray-500 w-16 h-16"/>
+                    <div className="flex-col flex self-end pb-1">
+                        <span className="text-gray-500 font-bold">{characterStatistics.defense}</span>
+                        <span className="text-white">Defense</span>
                     </div>
                 </div>
             </div>
