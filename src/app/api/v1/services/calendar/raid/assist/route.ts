14a9532a1dc27285eb26269491a40ca4d7f4814a
Yago <yarlorenzo@gmail.com>
2024-08-14 12:45:25 +0200

    feat: when declinging the next action will set the user as new record in the assist list

--- src/app/api/v1/services/calendar/raid/assist/route.ts
+++ src/app/api/v1/services/calendar/raid/assist/route.ts
@@ -6,13 +6,32 @@ import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
 
 async function registerOnRaid(characterId: string, raidId: string, details: any, supabase: any) {
 
+    let wasDeclined = false
+    if(details?.status !== 'declined') {
+        const {data: declinedParticipants, error} = await supabase.from('ev_raid_participant')
+            .select('member_id')
+            .eq('raid_id', raidId)
+            .eq('details->>status', 'declined')
+            .eq('member_id', characterId)
+            .single()
+
+        if (error && error.code !== 'PGRST116') {
+            console.error('Error fetching declined participants', error)
+            return {error}
+        }
+        if (declinedParticipants) {
+            wasDeclined = true
+        }
+    }
+
     return supabase.from('ev_raid_participant')
         .upsert({
             raid_id: raidId,
             member_id: characterId,
             updated_at: new Date(),
             details: details,
-            is_confirmed: details?.status === 'confirmed'
+            is_confirmed: details?.status === 'confirmed',
+            ...(wasDeclined ? {created_at: new Date()} : {})
         })
         .eq('raid_id', raidId)
         .eq('member_id', characterId)
