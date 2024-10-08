3f181dce1b1d23aa23bbe1c99d07d07143ac0ef4
Yago <yarlorenzo@gmail.com>
2024-10-08 19:25:48 +0200

    package updates+added share link to the raid view

--- src/app/raid/components/RaidOptions.tsx
+++ src/app/raid/components/RaidOptions.tsx
@@ -1,8 +1,25 @@
 'use client';
 import {Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBackward, faCalendarDay, faForward, faGear, faGift, faCartPlus} from "@fortawesome/free-solid-svg-icons";
+import {
+    faBackward,
+    faCalendarDay,
+    faForward,
+    faGear,
+    faGift,
+    faCartPlus,
+    faShareNodes
+} from "@fortawesome/free-solid-svg-icons";
 import {useRouter} from "next/navigation";
+
+const KEYS = {
+    NEXT: 'next',
+    PREVIOUS: 'previous',
+    CURRENT: 'current',
+    LOOT: 'loot',
+    SOFT_RESERV: 'soft-reserv',
+    SHARE: 'share'
+}
 
 export function RaidOptions({
                                 nextResetId,
@@ -28,30 +45,45 @@ export function RaidOptions({
             </DropdownTrigger>
             <DropdownMenu
                 onAction={(key) => {
-                    if (key === 'loot') {
+                    if (key === KEYS.LOOT) {
                         router.push(`/raid/${currentResetId}/loot`, {
                             scroll: false
                         })
                     }
-                    if (key === 'current') {
+                    if (key === KEYS.CURRENT) {
                         router.push(`/raid/current`)
                     }
 
-                    if (key === 'next') {
+                    if (key === KEYS.NEXT) {
                         router.push(`/raid/${nextResetId}`)
                     }
 
-                    if (key === 'previous') {
+                    if (key === KEYS.PREVIOUS) {
                         router.push(`/raid/${previousResetId}`)
                     }
-                    if(key === 'soft-reserv') {
+                    if(key === KEYS.SOFT_RESERV) {
                         router.push(`/raid/${currentResetId}/soft-reserv`)
+                    }
+                    if(key === KEYS.SHARE) {
+                        const url = window.location.href
+                        if(!navigator?.clipboard?.writeText) {
+                            alert('Could not copy link to clipboard')
+                            return
+                        }
+                        navigator.clipboard.writeText(url.substring(
+                            0,
+                            url.indexOf('-') - 1
+                        )).then(() => {
+                            alert('Link copied to clipboard')
+                        }).catch(() => {
+                            alert('Could not copy link to clipboard')
+                        })
                     }
                 }}
                 aria-label="Raid actions">
                 <DropdownItem
                     isDisabled={!nextResetId}
-                    key={'next'}
+                    key={KEYS.NEXT}
                     className="flex items-center gap-2 justify-between"
                 >
                     <div className="flex items-center gap-2 justify-between">
@@ -59,24 +91,37 @@ export function RaidOptions({
                     </div>
                 </DropdownItem>
                 <DropdownItem
-                    key={'previous'}
+                    key={KEYS.PREVIOUS}
                     isDisabled={!previousResetId}
                 >
                     <div className="flex items-center gap-2 justify-between">
                         Previous <FontAwesomeIcon icon={faBackward}/>
                     </div>
                 </DropdownItem>
-                <DropdownItem key={'current'}>
+                <DropdownItem key={KEYS.CURRENT}>
                     <div className="flex items-center gap-2 justify-between">
                         Current <FontAwesomeIcon icon={faCalendarDay}/>
                     </div>
                 </DropdownItem>
-
                 <DropdownItem
-                    key={'loot'}
+                    key={KEYS.LOOT}
                     isDisabled={!hasLoot}>
                     <div className="flex items-center gap-2 justify-between">
                         Loot <FontAwesomeIcon icon={faGift}/>
+                    </div>
+                </DropdownItem>
+                <DropdownItem
+                    key={KEYS.SOFT_RESERV}
+                    >
+                    <div className="flex items-center gap-2 justify-between">
+                        Soft Reserv <FontAwesomeIcon icon={faCartPlus}/>
+                    </div>
+                </DropdownItem>
+                <DropdownItem
+                    key={KEYS.SHARE}
+                >
+                    <div className="flex items-center gap-2 justify-between">
+                        Share link <FontAwesomeIcon icon={faShareNodes} />
                     </div>
                 </DropdownItem>
             </DropdownMenu>
