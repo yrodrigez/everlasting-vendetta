18c61c331a7a1584f6d1b1f2cc1483bd060f9764
Yago <yarlorenzo@gmail.com>
2025-01-07 14:41:21 +0100

    chore: moved the gearscore calculation to a supabase function

--- src/app/api/v1/services/member/character/[name]/gs/route.ts
+++ src/app/api/v1/services/member/character/[name]/gs/route.ts
@@ -1,6 +1,5 @@
 import {NextRequest, NextResponse} from "next/server";
 
-
 export async function GET(request: NextRequest, context: any) {
 	const params = await context.params;
 	const name = (params?.name ?? '').toLowerCase();
