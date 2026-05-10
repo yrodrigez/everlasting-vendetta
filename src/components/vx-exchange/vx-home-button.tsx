0c47964c590004b5c048b8cf1768df602bedc37e
Yago <yarlorenzo@gmail.com>
2026-05-11 01:20:01 +0200

    feat(vx): nav-button is now active when in path

--- src/components/vx-exchange/vx-home-button.tsx
+++ src/components/vx-exchange/vx-home-button.tsx
@@ -1,6 +1,9 @@
+'use client';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function VxHomeButton() {
+    const path = usePathname();
     const vxBlur = '/vx/ev-vx-off.webp';
     const vxHover = '/vx/ev-vx-on.webp';
     const className = "px-2 py-1 flex flex-col items-center rounded hover:cursor-pointer hover:bg-white hover:bg-opacity-20 backdrop-filter backdrop-blur-md min-w-16 max-w-16 transition-all duration-300 ease-in-out";
@@ -12,17 +15,16 @@ export default function VxHomeButton() {
         >
             <div className="relative w-9 h-9 rounded-full">
                 <img
-
                     width="36"
                     height="36"
                     alt="VX" src={vxBlur}
-                    className="group-hover:opacity-0 transition-all duration-300 ease-in-out absolute top-0 left-0 rounded-full w-9 h-9 min-h-9 max-h-9 min-w-9 max-w-9"
+                    className={ `transition-all duration-300 ease-in-out rounded-full w-9 h-9 min-h-9 max-h-9 min-w-9 max-w-9 ${path.startsWith("/vx") ? "opacity-0" : "opacity-100"}` }
                 />
                 <img
                     width="36"
                     height="36"
                     alt="VX" src={vxHover}
-                    className="opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out absolute top-0 left-0 rounded-full w-9 h-9 min-h-9 max-h-9 min-w-9 max-w-9"
+                    className={`opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out absolute top-0 left-0 rounded-full w-9 h-9 min-h-9 max-h-9 min-w-9 max-w-9 ${path.startsWith("/vx") ? "opacity-100" : ""}`}
                 />
             </div>
             <span
